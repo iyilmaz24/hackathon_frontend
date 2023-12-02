2faa866afb22d83399fbc7e2bddd7ab09a8e9509
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-02 16:00:10 -0500

    install npm dependencies, write webpack config file, npm scripts
    create template.html
    create index.js

---
+++ front-end/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HTMLPlugIn = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    module: 
+    {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+        ]
+    },
+    plugins: [new HTMLPlugIn({
+        title: 'Config HTMLPlugIn',
+        filename: 'index.html',
+        inject: 'head',
+        template: './src/template.html',
+      })],
+};
